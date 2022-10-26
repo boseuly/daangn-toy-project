684bd41fda4c057e2dbd4cfbd5028f0cb6dfe9b6
boseul <tool0923@naver.com>
2022-10-26 21:54:29 +0900

    Initial commit

---
+++ src/main/java/daangnmarket/daangntoyproject/DaangnToyProjectApplication.java
@@ -0,0 +1,13 @@
+package daangnmarket.daangntoyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnToyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaangnToyProjectApplication.class, args);
+	}
+
+}
