4f85f57a10f3cc5bfc4e186393f343a2f430f0d2
boseul <tool0923@naver.com>
2022-11-14 04:14:59 +0900

    feature : header 구현
    - common.html 파일에 header를 만들어 board-list와 home 페이지에 적용하였다.
    - spring security를 적용하기 위해서는 login페이지를 구현해야 한다.
    - login과 logout은 spring security를 사용하여 구현할 예정이다.

---
+++ src/main/java/daangnmarket/daangntoyproject/user/controller/AccountController.java
@@ -0,0 +1,10 @@
+package daangnmarket.daangntoyproject.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/account")
+public class AccountController {
+
+
+
+}
