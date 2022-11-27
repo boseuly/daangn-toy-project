756e2d38fb73a6f857d7dfa39159e905958a1aa6
boseul <tool0923@naver.com>
2022-11-27 23:11:33 +0900

    feature : header에 내 정보 드롭다운에 사용자 이미지가 뜨도록 구현
    - 사용자 이미지를 띄우기 위해 session에 사용자의 login 정보를 저장해두었다.

--- src/main/java/daangnmarket/daangntoyproject/home/controller/HomeController.java
+++ src/main/java/daangnmarket/daangntoyproject/home/controller/HomeController.java
@@ -1,21 +1,32 @@
 package daangnmarket.daangntoyproject.home.controller;
 
+import daangnmarket.daangntoyproject.user.domain.User;
 import daangnmarket.daangntoyproject.user.model.UserDto;
+import daangnmarket.daangntoyproject.user.repository.UserRepository;
 import daangnmarket.daangntoyproject.user.service.KakaoOAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
     @Autowired
     private KakaoOAuthService kakaoService;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping(value = "/")
-    public String home(){
+    public String home(Principal principal, HttpSession session){
+        UserDto login = null;
+        if(principal != null){
+            login = new UserDto(userRepository.findById(principal.getName()));
+        }
+        session.setAttribute("login", login);
         return "/home";
     }
 
--- src/main/java/daangnmarket/daangntoyproject/user/domain/User.java
+++ src/main/java/daangnmarket/daangntoyproject/user/domain/User.java
@@ -15,7 +15,6 @@ import java.util.List;
 public class User {
     @Id
     @Column(name = "user_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String userId;
 
     @Column(name = "user_password")
