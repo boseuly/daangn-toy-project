1aa173748ca98c1e33b7620f49c76afb724ffe70
boseul <tool0923@naver.com>
2023-01-01 01:43:00 +0900

    feature : script에서 : 를 기준으로 글자를 자를 때 대화내용에서 사용되는 :까지 잘라버리는 이슈 발생
    - 원래는 첫 번째로 사용되는 :를 기준으로 0번 인덱스는 회원명, 1번 인덱스는 대화 내용으로 구분을 했었다.
    - 대화 내용에 :가 있으면 :를 기준으로 대화내용을 잘라버려서 대화내용이 제대로 전달되지 않음. message = arr[1]; 로 전달을 했기 때문에 :를 기준으로 뒤에 내용은 전달이 안 됨
    - 아직 완벽한 해결 방법은 찾지 못 하였고 임시방편으로 message = arr.slice(1); 이렇게 해줬다.
    - 하지만 이렇게 하면 :를 기준으로 대화 내용이 잘라졌다가 다시 붙여진 것인데, :가 있던 부분은 , 으로 대체된다. 아마 빈공간이 ,으로 대체된 듯하다.

--- src/main/java/daangnmarket/daangntoyproject/config/ChatHandler.java
+++ src/main/java/daangnmarket/daangntoyproject/config/ChatHandler.java
@@ -1,6 +1,8 @@
 package daangnmarket.daangntoyproject.config;
 
+import daangnmarket.daangntoyproject.chat.service.ChatService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -13,12 +15,15 @@ import java.util.List;
 @Component
 @Log4j2
 public class ChatHandler extends TextWebSocketHandler { // text 기반의 채팅을 구현할 것 -> textwebsocketHandler 상속
+    @Autowired
+    private ChatService chatService;
     private static List<WebSocketSession> list = new ArrayList<>();
+
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
-        log.info("payload : " + payload);   // payload : 전송되는 데이터를 의미. JSON에서 페이로드는 data이다.
+        log.info("전달 메시지 내용 payload : " + payload);   // payload : 전송되는 데이터를 의미. JSON에서 페이로드는 data이다.
 
         for(WebSocketSession sess: list) {
             sess.sendMessage(message);
